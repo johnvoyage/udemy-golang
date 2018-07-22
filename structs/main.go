f073dfd7f8d012cb73a955cf01672ab32c0121b2
johnvoyage <jmyork762@gmail.com>
2018-07-22 14:04:05 -0400

    Learn structs

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,40 @@
 package main
 
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"}
+	// alex := person{"Alex", "Anderson"}
+
+	// alex := person{
+	// 	firstName: "Alex",
+	// 	lastName:  "Anderson",
+	// }
+
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Jones",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
